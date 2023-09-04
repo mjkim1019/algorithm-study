f7b099b683cfa6d9b2c0e97544a34867ed91d86a
Minju Kim <mindori1019@gmail.com>
2023-09-04 12:01:53 +0900

    시간초과 4090 4090_mjkim.cpp

--- 1주차/4090/4090_mjkim.cpp
+++ 1주차/4090/4090_mjkim.cpp
@@ -2,85 +2,93 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+#define FALSE "FALSE"
 
 string X;
+int N;
 
-bool vampire(int num, vector<int> & v){
-    int x = stoi(X);
-    if (x % num == 0){
-        int num2 = x / num;
-        while (num2 > 0){
-            int val = num2 % 10;
-            if (v[val]) v[val]--;
-            else return false;
-            num2 /= 10;
-        }
-        return true;
-    } 
-    return false;
+vector<string> per(vector<char> v)
+{
+    vector<string> res = {};
+
+    do
+    {
+        string s = "";
+        if (v[0] == '0') continue;
+        for (auto &e : v)
+            s += e;
+        res.push_back(s);
+
+    } while (next_permutation(v.begin(), v.end()));
+
+    return res;
 }
 
-bool check(int n){
-    vector<int> brute(X.size(), 1);
-    fill(brute.begin(), brute.begin()+n, 0);
-    do {
-        vector<int> v1;
-        vector<int> v2(10);
-        for (int i=0; i<X.size(); i++){
-            if (brute[i] == 0) {
-                v1.push_back(X[i]-'0');
+string select(int n)
+{
+    vector<int> brute(N, 1);
+    fill(brute.begin(), brute.begin() + n, 0);
+
+    // 숫자 선택
+    do
+    {
+        vector<char> v1;
+        vector<char> v2;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (brute[i] == 0)
+                v1.push_back(X[i]);
+            else
+                v2.push_back(X[i]);
+        }
+
+        vector<string> g1 = per(v1);
+        vector<string> g2 = per(v2);
+
+        int a, b;
+        for (string e1 : g1)
+        {
+            a = stoi(e1);
+            for (string e2 : g2)
+            {
+                b = stoi(e2);
+
+                if (a * b == stoi(X))
+                    return X;
             }
-            else v2[X[i]-'0']++;
         }
-        if (v1[0] == 0) continue;
-        if (v1.size() > 1){
-            do{
-                int num = 0;
-                int tmp = 1;
-                for (int i=v1.size()-1; i >=0; i--){
-                    num += tmp * v1[i];
-                    tmp *=10;
-                }   
-                //cout << "num = " << num <<'\n';
 
-                if (vampire(num, v2)) return true;
-
-            }while (next_permutation(v1.begin(), v1.end()));
-        } else {
-            if (vampire(v1[0], v2)) return true;
-        }
-        
     } while (next_permutation(brute.begin(), brute.end()));
 
-    return false;
+    return FALSE;
 }
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     while (true){
-        int N;
-        cin >> N;
-        if (N == 0) return 0;
-
-        bool is_found = false;
-        while (!is_found){
-            X = to_string(N);
-            int n = X.size() / 2;
-            while (n >= 1){
-                if (check(n)){
-                    is_found = true;
-                    break;
-                }
-                n--;
-            }
-            if (is_found) {
-                cout << N << '\n';
-                break;
-            }
-            else N++;
+    int V;
+    cin >> V;
+    if (V == 0) break;
+    bool found = false;
+    while (!found)
+    {
+        X = to_string(V);
+        N = X.size();
+        for (int i = 1; i <= N / 2; i++)
+        {
+            string res = select(i);
+            if (res == FALSE) continue;
+            cout << res << '\n';
+            found = true;
+            break;
         }
+
+        if (!found) V++;
+    }
     }
 
     return 0;
