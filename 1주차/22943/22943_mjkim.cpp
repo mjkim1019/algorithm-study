0219945db99e1bf6af0c2af15b52b147b9e5fcb0
Minju Kim <mindori1019@gmail.com>
2023-09-07 02:58:38 +0900

    시간초과 22943 mjkim.cpp

---
+++ 1주차/22943/22943_mjkim.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<bool> prime(100000, true);
+vector<bool> res(100000, false);
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int answer = 0;
+
+    int K, M;
+    cin >> K >> M;
+    // 1. 숫자 만들기
+    vector<int> arr;
+    vector<int> brute(10, 1);
+    fill(brute.begin(), brute.begin()+K, 0);
+    do {
+        vector<int> select;
+        for (int i=0; i<10; i++) if (brute[i] == 0) select.push_back(i);
+
+        do{
+            string s = "";
+            if (select[0] == 0) continue;
+            for (int i=0; i<K; i++){
+                s += select[i] + '0';
+            }
+            //cout << s << '\n';
+            arr.push_back(stoi(s));
+        }while (next_permutation(select.begin(), select.end()));
+    } while (next_permutation(brute.begin(), brute.end()));
+
+    // 2. 소수 찾기
+    sort(arr.begin(), arr.end());
+    prime[0] = false; prime[1] = false;
+    for (int i=2; i*i <= arr.back(); i++){
+        if (prime[i] == false) continue;
+        for (int j=2; i*j <=arr.back(); j++){
+            prime[i*j] = false;
+        }
+    }
+
+    vector<int> prime_num;
+    for (int i=2; i<= arr.back(); i++){
+        if (prime[i]) prime_num.push_back(i); // 정렬된 상태로 들어감
+    }
+
+    // 3. 조건 1
+    for (int &n: arr){
+        for (int &p: prime_num){
+            if (n-p <= 0) break;
+
+            if (binary_search(prime_num.begin(), prime_num.end(), n-p)) {
+                if (n-p != p) res[n] = true;
+                break;
+            }
+        }
+    }
+
+    // 4. 조건 2
+    for (int &n: arr){
+        int tmp = n;
+        while (tmp%M == 0) tmp /= M;
+        for (int &p: prime_num) {
+            if (tmp % p != 0) continue;
+            if (binary_search(prime_num.begin(), prime_num.end(), tmp/p)){
+                if (res[n]) answer++;
+                break;
+            }
+        }
+    }
+    cout << answer;
+
+
+    return 0;
+}
