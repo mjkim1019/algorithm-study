abb7c5fd9ba9a1227b95473f794a3a6722c7d9b0
Minju Kim <mindori1019@gmail.com>
2023-09-08 18:27:55 +0900

    못풀겠음!!! 2159 mjkim.cpp

--- 1주차/2159/2159_mjkim.cpp
+++ 1주차/2159/2159_mjkim.cpp
@@ -1,73 +1,63 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <algorithm>
 using namespace std;
-#define X first
+#define X first 
 #define Y second
+#define INF 0x7f7f7f7f
+#define ll long long
 
 int N;
-vector<pair<int, int>> arr;
+pair<int, int> pos[100005][5];
+ll d[100005][5];
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {1, -1, 0, 0};
+int dx[5] = {0, 1, -1, 0, 0};
+int dy[5] = {0, 0, 0, -1, 1};
 
-int main()
-{
+bool OOB(int x, int y){
+    if (x < 1 || x > 100000 || y <1 || y > 100000) return true;
+    return false;
+}
+
+ll dist(int x, int y, int a, int b) {
+    return abs(x-a) + abs(y-b);
+}
+
+ll move(int idx, int dir){
+    if (idx == N) return 0;
+    if (d[idx][dir] != INF) return d[idx][dir];
+
+    d[idx][dir] = 0;
+    ll mv = INF;
+    for (int i=0; i<=4; i++){
+        if (pos[idx+1][i].X == INF) continue;
+
+        mv = min(mv, move(idx+1, i) + dist(pos[idx][dir].X, pos[idx][dir].Y, pos[idx+1][i].X, pos[idx+1][i].Y));
+    }
+
+    return d[idx][dir] += mv;
+}
+
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> N;
-    int r, c;
-    cin >> r >> c;
-    int mx_x = 0, mx_y = 0;
-    for (int i = 0; i < N; i++)
-    {
-        int a, b;
+    int a, b;
+    cin >> pos[0][0].X >> pos[0][0].Y;
+    for (int i=1; i<=4; i++) pos[0][i] = {INF, INF};
+    for (int i=1; i<=N; i++){
         cin >> a >> b;
-        mx_x = max(mx_x, a);
-        mx_y = max(mx_y, b);
-        arr.push_back({a, b});
+        for (int dir=0; dir<=4; dir++){
+            int nx = a + dx[dir];
+            int ny = b+ dy[dir];
+            if (OOB(nx, ny)) pos[i][dir] = {INF, INF};
+            else pos[i][dir] = {nx, ny};
+        }
     }
     
-    vector<vector<int>> d(mx_x+1, vector<int>(mx_y+1, -1));
-    d[r][c] = 0;
-    queue<pair<int, int>> Q;
-    Q.push({r, c});
-    while (!Q.empty())
-    {
-        auto cur = Q.front(); Q.pop();
-        //cout << cur.X << ", " << cur.Y << '\n';
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if (nx < 0 || nx > mx_x || ny < 0 || ny > mx_y) continue;
-            if (d[nx][ny] >= 0) continue;
-            d[nx][ny] = d[cur.X][cur.Y] + 1;
-            Q.push({nx, ny});
-        }
-    }
-
-    pair<int, int> pos;
-    pair<int, int> ex = {r, c};
-    int answer = 0;
-    for (auto &cur : arr)
-    {
-        pos = cur;
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if (nx < 0 || nx > mx_x || ny < 0 || ny > mx_y) continue;
-
-            if (d[nx][ny] < d[pos.X][pos.Y]) pos = {nx, ny};
-        }
-        cout << pos.X  << "," << pos.Y << '\n';
-        cout << d[pos.X][pos.Y] << "-" << d[ex.X][ex.Y] << "= " << d[pos.X][pos.Y] - d[ex.X][ex.Y] << '\n';
-        answer += abs(d[pos.X][pos.Y] - d[ex.X][ex.Y]);
-        ex = pos;
-    }
-    cout << answer;
+    for (int i=0; i<=N; i++) fill(d[i], d[i]+5, INF);
+    cout << move(0, 0);
+    
 
     return 0;
 }
