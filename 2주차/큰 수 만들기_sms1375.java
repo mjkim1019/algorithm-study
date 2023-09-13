6f336d620a9f9b2932f7683ba87d7139fce6e3a3
sms1375 <102645960+sms1375@users.noreply.github.com>
2023-09-13 17:19:12 +0900

    Create 큰 수 만들기_sms1375.java

---
+++ 2주차/큰 수 만들기_sms1375.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for(int i = 0; i < number.length() - k; i++) {
+            max = 0;
+            for(int j = index; j <= k + i; j++) {
+                if(max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    index = j + 1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
