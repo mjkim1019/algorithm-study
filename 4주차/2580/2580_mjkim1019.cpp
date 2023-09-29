07c2d4a7a2f579f04fad90e33b2f8eeeae5db36c
Minju Kim <mindori1019@gmail.com>
2023-09-30 03:07:45 +0900

    Solved 2580 2580_mjkim1019.cpp
    재귀는 생각해내기 쉽지 않군요.. n번째 값을 구하면 n+1번째 값도 구할 수 있다!! 스도쿠는 전형적인 재귀 문제!

--- 4주차/2580/2580_mjkim1019.cpp
+++ 4주차/2580/2580_mjkim1019.cpp
@@ -1,69 +1,54 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#define X first
+#define Y second
 
+vector<pair<int, int>> blank;
 int board[10][10];
-
-bool check_raw(int k){
-    vector<bool> num(10, false);
-    vector<int> v;
-    for (int i=0; i<9; i++){
-        if (board[i][k] == 0) v.push_back(i);
-        else num[board[i][k]] = true;
-    }
-    if (v.size() == 0) return true;
-    else if (v.size() > 1) return false;
-    for (int i=1; i<=9; i++) {
-        if (num[i] == false) {
-            board[v[0]][k] = i;
-        }
-    }
-    return true;
-}
-
-bool check_col(int k){
-    vector<bool> num(10, false);
-    vector<int> v;
-    for (int j=0; j<9; j++){
-        if (board[k][j] == 0) v.push_back(j);
-        else num[board[k][j]] = true;
-    }
-    //cout << k <<"행 : "<< cnt << '\n';
-    if (v.size() == 0) return true;
-    else if (v.size() > 1) return false;
-    for (int i = 1; i <= 9; i++){
-        if (num[i] == false) {
-            board[k][v[0]] = i;
-        }
-    }
-    return true;
-}
-
-bool check_square(int x, int y){
-    vector<pair<int, int>> v;
-    vector<bool> num(10, false);
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            if (board[x+i][y+j] == 0) v.push_back({x+i, y+j});
-            else num[board[x+i][y+j]] = true;
-        }
-    }
-    if (v.size() == 0) return true;
-    else if (v.size() > 1) return false;
-    for (int i = 1; i <= 9; i++){
-        if (num[i] == false) {
-            board[v[0].first][v[0].second] = i;
-        }
-    }
-    return true;
-}
+bool is_found = false;
 
 void print(){
-    for (int i=0; i<9; i++){
-        for (int j=0; j<9; j++){
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
             cout << board[i][j] << ' ';
         }
         cout << '\n';
+    }
+}
+
+bool is_valid(int num, int x, int y){
+    for (int i = 0; i < 9; i++){
+        if (num == board[i][y]) return false; // 열 체크  
+        if (num == board[x][i]) return false; // 행 체크
+    }
+    int st_x = x / 3;
+    int st_y = y / 3;
+    for (int i = st_x * 3; i < st_x * 3 + 3; i++){
+        for (int j = st_y * 3; j < st_y * 3 + 3; j++){
+            if (num == board[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+// 재귀 : n번째를 값을 구하면 n+1번째 값도 구할 수 있다
+// 몇 번(L)째 빈칸의 값을 1~9 값 중 하나로 정하고 가능한지 검사하는 함수
+void func(int L){
+    if (blank.size() == L){
+        print();
+        is_found = true;
+        return;
+    }
+
+    int x = blank[L].X;
+    int y = blank[L].Y;
+    for (int i = 1; i <= 9; i++){
+        if (!is_valid(i, x, y)) continue;
+        board[x][y] = i;
+        func(L + 1);
+        if (is_found) return;
+        board[x][y] = 0;
     }
 }
 
@@ -71,26 +56,14 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i=0; i<9; i++)
-        for (int j=0; j<9; j++)
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
             cin >> board[i][j];
-    //cout << "----------------------" << '\n';
-
-    bool col, raw, square;
-    while(true){
-        col = raw = square = true;
-        for (int i=0; i<9; i++) if (!check_col(i)) col = false; 
-        for (int j=0; j<9; j++) if (!check_raw(j)) raw = false;
-        for (int i=0; i<=6; i+=3){
-            for (int j=0; j<=6; j+=3){
-                if (!check_square(i, j)) square = false;
-            }
+            if (board[i][j] == 0) blank.push_back({i, j});
         }
-        if (col && raw && square) break;
-        // print();
-        // cout << "----------------------"<<'\n';
     }
-    print();
+
+    func(0);
 
     return 0;
 }
