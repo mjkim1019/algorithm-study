809c517b5df8a59c80aa4a8c4ea663dce29e13d2
Minju Kim <mindori1019@gmail.com>
2023-10-05 18:42:27 +0900

    Solved 11000 11000_mjkim1019.cpp
    들어오는 것과 나가는 것 각각을 하나의 원소로 만든다음 시간 순으로 정렬, 0과 1로 구분

--- 5주차/11000/11000_mjkim1019.cpp
+++ 5주차/11000/11000_mjkim1019.cpp
@@ -10,24 +10,22 @@ int main(){
     int answer = 0;
 
     int N; cin >> N;
-    vector<tuple<int, int, int>> v; // 시간, 인덱스, 타입(시작:1, 끝:0)
+    vector<tuple<int, int, int>> v; // 시간, 타입(시작:1, 끝:0), 인덱스
     for (int i=0; i<N; i++){
         int st, en;
         cin >> st >> en;
-        v.push_back({st, i, 1});
-        v.push_back({en, i, 0});
+        v.push_back({st, 1, i});
+        v.push_back({en, 0, i});
     }
     sort(v.begin(), v.end());
 
     set<int> s; // 진행중인 수업의 인덱스
     for (int i=0; i<v.size(); i++){
-        int t, idx, type;
-        tie(t, idx, type) = v[i];
-        if (type == 1){
-            s.insert(idx);
-        }else {
-            s.erase(idx);
-        }   
+        int t, type, idx;
+        tie(t, type, idx) = v[i];
+        if (type == 1) s.insert(idx);
+        else s.erase(idx);
+           
         int cnt = s.size();
         answer = max(cnt, answer);
     }
